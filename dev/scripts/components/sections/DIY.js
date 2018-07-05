fa039d3a3c4d74d4154e9d5fe3fc257eda0771b5
Melody Yun Chen <Melodyyunc@gmail.com>
2018-07-04 22:38:39 -0400

    fixed typo

--- dev/scripts/components/sections/DIY.js
+++ dev/scripts/components/sections/DIY.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import firebase from 'firebase';
+import firebaseui  from 'firebaseui';
 import {firebaseConfig} from "../../firebase/firebase-config";
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+const auth = firebase.auth();
 
 class DIY extends React.Component {
   constructor() {
-    super();                                                                     this.state = {
+    super();                                                                     
+    this.state = {
       loggedIn: false,
       userId: "",
       userName: "",
       userImg: "",
     }
     this.loginWithGoogle = this.loginWithGoogle.bind(this);
+    this.login = this.login.bind(this);
   };
 
   //googleLogin
+
+  login() {
+    const uiConfig = {
+      callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+          // User successfully signed in.
+          // Return type determines whether we continue the redirect automatically
+          // or whether we leave that to developer to handle.
+          return true;
+        },
+        uiShown: function () {
+          // The widget is rendered.
+          // Hide the loader.
+          document.getElementById('loader').style.display = 'none';
+        }
+      },
+      // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+      signInFlow: 'popup',
+      signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      ],
+    };
+  }
+
 
   loginWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
@@ -63,7 +95,7 @@ class DIY extends React.Component {
       {this.state.loggedIn === true ? (
         <button onClick={this.logout}> Log Out </button>
       ): (
-        <button onClick={this.loginWithGoogle}> Login </button>
+        <button onClick={this.login}> Login </button>
       )}
       </React.Fragment>
     )

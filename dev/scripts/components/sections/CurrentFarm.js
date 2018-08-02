bc866b75a1f7a337910b632902cb4a3eb7e973d5
Melody Yun Chen <Melodyyunc@gmail.com>
2018-08-01 22:03:17 -0400

    added checkbox for past and current farm

--- dev/scripts/components/sections/CurrentFarm.js
+++ dev/scripts/components/sections/CurrentFarm.js
@@ -2,10 +2,22 @@ import React from "react";
 import AddProduceForm from "./AddProduceForm";
 
 class CurrentFarm extends React.Component {
+  constructor() {
+    super();
+  }
+  //if move to past farm is check
+  //send alert
+  //alert return true
+  //rewrite all info from current farm to past farm
+  //delete info from current farm.
   render() {
     return (
       <div className="past-farms">
         <h2>Current Farm Cycle</h2>
+        <form>
+          <label name="past-farm">Move to past farm</label>
+          <input type="checkbox" name="past-farm"/>
+        </form>
         <AddProduceForm />
       </div>
     );
